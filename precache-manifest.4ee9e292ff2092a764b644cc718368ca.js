843684c065d74161f97f191ff9413fa518a94716
hriczzoli <hriczzoli@gmail.com>
2020-02-23 21:20:58 +0000

    Updates

---
+++ precache-manifest.4ee9e292ff2092a764b644cc718368ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2de9734f32eea3989e22064bdf8f9c88",
+    "url": "/Smart-Home/index.html"
+  },
+  {
+    "revision": "7f0e6bc4ea085d2e572c",
+    "url": "/Smart-Home/static/css/main.b7f75ff0.chunk.css"
+  },
+  {
+    "revision": "f377040e1fd39b97557b",
+    "url": "/Smart-Home/static/js/2.8f944ffc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Smart-Home/static/js/2.8f944ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f0e6bc4ea085d2e572c",
+    "url": "/Smart-Home/static/js/main.994f58d9.chunk.js"
+  },
+  {
+    "revision": "a928efffdb7c2bde9859",
+    "url": "/Smart-Home/static/js/runtime-main.c9115710.js"
+  }
+]);
